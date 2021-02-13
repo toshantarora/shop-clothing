5a4bbfdc8ee9827228313e7b30d7645058bd778f
Toshant <42293092+toshantarora@users.noreply.github.com>
2021-02-13 13:40:58 +0530

    redirect homepage if signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
-import {Switch, Route } from 'react-router-dom';
+import {Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -57,15 +57,20 @@ class App extends Component {
       <Switch> 
       <Route exact path= '/' component={HomePage}></Route>
       <Route path= '/shop' component={ShopPage}></Route>
-      <Route path= '/signIn' component={SignInAndSignUp}></Route>
+      <Route exact path= '/signIn' render={() => this.props.currentUser ? (<Redirect to= '/' />) : <SignInAndSignUp/>}></Route>
   </Switch>
       </div>
     );
   }
   
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser : user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
